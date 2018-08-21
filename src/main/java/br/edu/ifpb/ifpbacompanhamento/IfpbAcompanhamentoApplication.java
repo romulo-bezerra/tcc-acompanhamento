775b6b99649eccfa6302dc5f5237890d6c45cb39
romulo-soares <bromulosoares@gmail.com>
2018-08-20 23:34:33 -0300

    Start - Ambiente Spring configurado

---
+++ src/main/java/br/edu/ifpb/ifpbacompanhamento/IfpbAcompanhamentoApplication.java
@@ -0,0 +1,12 @@
+package br.edu.ifpb.ifpbacompanhamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfpbAcompanhamentoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IfpbAcompanhamentoApplication.class, args);
+    }
+}
---
+++ src/test/java/br/edu/ifpb/ifpbacompanhamento/IfpbAcompanhamentoApplicationTests.java
@@ -0,0 +1,16 @@
+package br.edu.ifpb.ifpbacompanhamento;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IfpbAcompanhamentoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
