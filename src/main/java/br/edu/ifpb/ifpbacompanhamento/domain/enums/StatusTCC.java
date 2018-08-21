bd3bd68cee8a2074e77730cccf48d11338c1b940
romulo-soares <bromulosoares@gmail.com>
2018-08-21 10:34:07 -0300

    Entity TCC e application.properties

---
+++ src/main/java/br/edu/ifpb/ifpbacompanhamento/domain/enums/StatusTCC.java
@@ -0,0 +1,12 @@
+package br.edu.ifpb.ifpbacompanhamento.domain.enums;
+
+public enum StatusTCC {
+
+    EM_ANDAMENTO,
+    ENTREGUE,
+    NAO_ENTREGUE,
+    EM_REVISAO,
+    APROVADO,
+    REPROVADO
+
+}
---
+++ src/main/java/br/edu/ifpb/ifpbacompanhamento/domain/enums/TipoTCC.java
@@ -0,0 +1,9 @@
+package br.edu.ifpb.ifpbacompanhamento.domain.enums;
+
+public enum TipoTCC {
+
+    TCC1,
+    TCC2,
+    TCC_UNICO
+
+}
