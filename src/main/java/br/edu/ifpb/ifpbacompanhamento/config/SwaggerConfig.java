8234a97e3be59a07d2d5d635eb537a3436971f75
romulo-soares <bromulosoares@gmail.com>
2018-08-23 01:27:13 -0300

    Config swagger atualizada e add do liquibase

--- src/main/java/br/edu/ifpb/ifpbacompanhamento/config/SwaggerConfig.java
+++ src/main/java/br/edu/ifpb/ifpbacompanhamento/config/SwaggerConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -20,7 +21,25 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("br.edu.ifpb.ifpbacompanhamento.web"))
                 .paths(regex("/api.*"))
-                .build();
+                .build()
+                .apiInfo(metaInfo());
     }
 
+    private ApiInfo metaInfo() {
+
+        ApiInfo apiInfo = new ApiInfo(
+                "IFPB-Serviço |TCC-Acompanhamento",
+                "API TCC-Acompanhamento",
+                "",
+                "",
+                new Contact("IFPB-TCC-Acompanhamento", "https://github.com/ifpb/TCC-IFPB/tree/master/TCC-Acompanhamento",
+                        "bromulosoares@gmail.com"),
+                "Apache License Version 2.0",
+                "http://www.apache.org/licenses/LICENSE-2.0"
+        );
+
+        return apiInfo;
+    }
 }
+
+
