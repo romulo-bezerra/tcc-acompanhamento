857f97f9d7e026ec5cbafdef9bf09de85b2bd3b4
romulo-soares <bromulosoares@gmail.com>
2018-09-06 00:52:22 -0300

    gateway adicionado

--- src/main/java/br/edu/ifpb/ifpbacompanhamento/domain/enums/TipoEntrega.java
+++
@@ -1,8 +0,0 @@
-package br.edu.ifpb.ifpbacompanhamento.domain.enums;
-
-public enum TipoEntrega {
-
-    DOCUMENTO_DEFESA,
-    DOCUMENTO_FINAL
-
-}
--- src/main/java/br/edu/ifpb/ifpbacompanhamento/domain/enums/TipoTCC.java
+++
@@ -1,9 +0,0 @@
-package br.edu.ifpb.ifpbacompanhamento.domain.enums;
-
-public enum TipoTCC {
-
-    TCC1,
-    TCC2,
-    TCC_UNICO
-
-}
